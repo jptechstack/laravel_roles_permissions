609fc5afa8bd0de6436d88fa355b69647f12d7f0
jptechstack <jptechstack@gmail.com>
2025-06-16 19:55:05 -0300

    ajustando a view edit permission

--- resources/views/permissions/edit.blade.php
+++ resources/views/permissions/edit.blade.php
@@ -2,9 +2,9 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Permissions / Create
+                Permissions / Edit
             </h2>
-            <a href="{{route('permissions.index')}}" class="bg-slate-700 text-sm rounded-md px-3 py-2 text-white">Back</a>
+            <a href="{{route('permissions.edit')}}" class="bg-slate-700 text-sm rounded-md px-3 py-2 text-white">Back</a>
         </div>
     </x-slot>
 
@@ -17,7 +17,7 @@
                         <div>
                             <label for="" class="text-lg font-medium">Name</label>
                             <div class="my-3">
-                                <input value="{{old('name')}}" placeholder="Enter Name" type="text" name="name" id="name" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
+                                <input value="{{old('name', $permission->name)}}" placeholder="Enter Name" type="text" name="name" id="name" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
                                 @error('name')
                                     <p class="text-red-400 font-medium">{{$message}}</p>
                                 @enderror
