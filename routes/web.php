1469adfefec8268f1328625fdc363d9cf75d1e7d
jptechstack <jptechstack@gmail.com>
2025-06-16 19:56:53 -0300

    adiciona a rota de edição

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions.index');
     Route::get('/permissions/create', [PermissionController::class, 'create'])->name('permissions.create');
     Route::post('/permissions', [PermissionController::class, 'store'])->name('permissions.store');
+    Route::get('/permissions/{id)/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
 });
 
 require __DIR__.'/auth.php';
