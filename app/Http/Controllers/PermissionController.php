21853e1d6831c00277e506aed96d9d246cc2fce9
jptechstack <jptechstack@gmail.com>
2025-06-16 17:25:45 -0300

    criando controller permissions

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    //this method will show permissions page
+    public function index() {
+
+    }
+
+    //this method will show create permissions page
+    public function create() {
+
+    }
+
+    //this method will insert a permissions in DB
+    public function store() {
+
+    }
+
+    //this method will show edit permissions page
+    public function edit() {
+
+    }
+
+    //this method will update a permissions
+    public function update() {
+
+    }
+
+    //this method will delete a permissions in DB
+    public function destroy() {
+
+    }
+}
