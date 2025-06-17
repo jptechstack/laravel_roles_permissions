3b07af4d55a6d0968d03836a0ee7477aaac30a46
jptechstack <jptechstack@gmail.com>
2025-06-16 22:57:19 -0300

    criado o RoleContoller

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    //this method will show roles page
+    public function index() {
+
+    }
+
+    //this method will create roles page
+    public function create() {
+
+    }
+
+
+    //this method will insert a role in DB
+    public function store() {
+
+    }
+
+
+}
