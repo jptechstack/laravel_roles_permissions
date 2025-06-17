615827e4998c634df545d91c1cda8db7a5c64b4a
jptechstack <jptechstack@gmail.com>
2025-06-16 23:31:10 -0300

    adicionando o metodo store do RoleController

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
@@ -24,8 +26,26 @@ class RoleController extends Controller
 
 
     //this method will insert a role in DB
-    public function store() {
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|unique:roles|min:3'
+        ]);
 
+        if($validator->passes()) {
+
+            $role = Role::create(['name' => $request->name]);
+
+            if(!empty($request->permission)) {
+                foreach ($request->permission as $name) {
+                    $role->givePermissionTo($name);
+                }
+            }
+
+            return redirect()->route('roles.index')->with('success', 'Role added successfully.');
+
+        } else {
+            return redirect()->route('roles.create')->withInput()->withErrors($validator);
+        }
     }
 
 
