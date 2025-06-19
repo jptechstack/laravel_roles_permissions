d518975ec21e42ae77c622c474e01a31adcd1216
jptechstack <jptechstack@gmail.com>
2025-06-19 17:19:45 -0300

    adicionando o template da view list article

---
+++ resources/views/articles/list.blade.php
@@ -0,0 +1,69 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Articles') }}
+            </h2>
+            <a href="{{route('articles.create')}}" class="bg-slate-700 text-sm rounded-md px-3 py-2 text-white">Create</a>
+        </div>
+    </x-slot>
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+               <x-message></x-message>
+               <table class="w-full">
+                    <thead class="bg-gray-50">
+                        <tr class="border-b">
+                            <th class="px-6 py-3 text-left" width="60">#</th>
+                            <th class="px-6 py-3 text-left">Name</th>
+                            <th class="px-6 py-3 text-left" width="180">Created</th>
+                            <th class="px-6 py-3 text-center" width="180">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white">
+                        @if ($permissions->isNotEmpty())
+                        @foreach ($permissions as $permission)
+                            <tr class="border-b">
+                                <td class="px-6 py-3 text-left">{{$permission->id}}</td>
+                                <td class="px-6 py-3 text-left">{{$permission->name}}</td>
+                                <td class="px-6 py-3 text-left">{{\Carbon\Carbon::parse($permission->created_at)->format('d/m/Y')}}</td>
+                                <td class="px-6 py-3 text-center">
+                                    <a href="{{route("permissions.edit", $permission->id)}}" class="bg-slate-700 text-sm rounded-md px-3 py-2 text-white hover:bg-slate-600">Edit</a>
+                                    <a href="javascript:void();" onclick="deletePermission( {{$permission->id}})" class="bg-red-600 text-sm rounded-md px-3 py-2 text-white hover:bg-red-500">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        @endif
+                    </tbody>
+               </table>
+            </div>
+            <div class="my-3">
+                {{ $permissions->links() }}
+            </div>
+        </div>
+    </div>
+
+    <x-slot name="script">
+        <script type="text/javascript">
+            function deletePermission(id) {
+                if(confirm("Are you sure want to delete?")) {
+                    $.ajax({
+                        url: '{{ route("permissions.destroy")}}',
+                        type: 'delete',
+                        data: {id:id},
+                        datatype: 'json',
+                        headers: {
+                            'x-csrf-token' : '{{ csrf_token() }}'
+                        },
+                        success: function(response) {
+                            window.location.href = '{{route("permissions.index")}}';
+                        }
+                    });
+                }
+            }
+        </script>
+    </x-slot>
+</x-app-layout>
