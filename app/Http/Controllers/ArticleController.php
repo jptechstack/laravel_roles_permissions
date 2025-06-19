6d507d385f3174f0511830da5f55fe38acbf418c
jptechstack <jptechstack@gmail.com>
2025-06-19 17:07:21 -0300

    validando inputs e salvando os registros no metodo store do ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
@@ -17,6 +19,24 @@ class ArticleController extends Controller
     }
 
     public function store( Request $request) {
+
+        $validador = Validator::make($request->all(), [
+            'title' => 'required|min:5',
+        ]);
+
+        if($validador->passes()) {
+
+            $article = new Article();
+            $article->title = $request->title;
+            $article->text = $request->text;
+            $article->author = $request->author;
+            $article->save();
+
+            return redirect()->routes('articles.index')->with('success', 'Articles added successfully.');
+
+        } else {
+            return redirect()->routes('articles.create')->withInput()->withErrors($validador);
+        }
 
     }
 
