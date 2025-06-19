d23b7c67fee8f528dfecbf1652382b70ee4165c4
jptechstack <jptechstack@gmail.com>
2025-06-19 14:29:27 -0300

    adicionando inputs e ajustando o form create articles

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Permissions / Create
+                Articles / Create
             </h2>
             <a href="{{route('permissions.index')}}" class="bg-slate-700 text-sm rounded-md px-3 py-2 text-white">Back</a>
         </div>
@@ -15,10 +15,23 @@
                     <form action="{{route('permissions.store')}}" method="POST">
                         @csrf
                         <div>
-                            <label for="" class="text-lg font-medium">Name</label>
+                            <label for="" class="text-lg font-medium">Title</label>
                             <div class="my-3">
-                                <input value="{{old('name')}}" placeholder="Enter Name" type="text" name="name" id="name" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
-                                @error('name')
+                                <input value="{{old('title')}}" placeholder="Title" type="text" name="title" id="title" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
+                                @error('title')
+                                    <p class="text-red-400 font-medium">{{$message}}</p>
+                                @enderror
+                            </div>
+
+                            <label for="" class="text-lg font-medium">Content</label>
+                            <div class="my-3">
+                                <textarea placeholder="Content" class="border-gray-300 shadow-sm w-1/2 rounded-lg" name="text" id="text" cols="30" rows="10">{{old('text')}}</textarea>
+                            </div>
+
+                            <label for="" class="text-lg font-medium">Author</label>
+                            <div class="my-3">
+                                <input value="{{old('author')}}" placeholder="Author" type="text" name="author" id="author" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
+                                @error('author')
                                     <p class="text-red-400 font-medium">{{$message}}</p>
                                 @enderror
                             </div>
