5b634b7f48dceb47caa4e0d9fb8161f335b24988
jptechstack <jptechstack@gmail.com>
2025-06-26 15:49:11 -0300

    adicionando a rota store users

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::middleware('auth')->group(function () {
     // User routes
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    // Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
     Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::post('/users/{id}', [UserController::class, 'update'])->name('users.update');
     // Route::delete('/users', [UserController::class, 'destroy'])->name('users.destroy');
